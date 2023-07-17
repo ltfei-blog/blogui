c443f583891e1c4e64830c58052dbc643d162b05
ltfei <ltfei@ltfei.cn>
2023-07-17 13:18:01 +0800

    style: 修改格式化配置

--- play/app.example.vue
+++ play/app.example.vue
@@ -8,5 +8,4 @@ defineOptions({
   <h1>在这里引入和测试组件</h1>
 </template>
 
-<style lang="less" scoped>
-</style>
+<style lang="less" scoped></style>
--- play/main.ts
+++ play/main.ts
@@ -2,4 +2,3 @@ import { createApp } from 'vue'
 import App from './src/App.vue'
 
 createApp(App).mount('#app')
- 

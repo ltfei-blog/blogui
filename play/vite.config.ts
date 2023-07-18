6353d32e63127b4ae54cf1203b9147d64cb1bebd
ltfei <ltfei@ltfei.cn>
2023-07-18 15:03:59 +0800

    chore: inspect插件

--- play/vite.config.ts
+++ play/vite.config.ts
@@ -1,7 +1,8 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import './vite.init'
+import Inspect from 'vite-plugin-inspect'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue(), Inspect()]
 })

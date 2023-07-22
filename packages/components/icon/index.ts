0ae64f1075e3d0a5e1d66e445e8ab6129d2e1808
ltfei <ltfei@ltfei.cn>
2023-07-22 20:31:59 +0800

    feat(components/icon): icon组件

---
+++ packages/components/icon/index.ts
@@ -0,0 +1,6 @@
+import Icon from './src/icon.vue'
+import { withInstall } from '@ltfei-blog/blogui-utils/vue/install'
+
+export const BIcon = withInstall(Icon)
+
+export default BIcon
---
+++ packages/components/icon/src/icon.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import type { CSSProperties } from 'vue'
+defineOptions({
+  name: 'BIcon'
+})
+
+const props = defineProps<{
+  size?: number | string
+  color?: string
+}>()
+
+const style = computed<CSSProperties>(() => {
+  const { size, color } = props
+  if (!size && !color) return {}
+
+  return {
+    fontSize: typeof size == 'string' ? size : size + 'px',
+    color: color
+  }
+})
+</script>
+
+<template>
+  <i class="b-icon" :style="style" v-bind="$attrs">
+    <slot />
+  </i>
+</template>

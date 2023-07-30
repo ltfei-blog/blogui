ca767cc4ca1df70f389d810bce55fbea7947829c
ltfei <ltfei@ltfei.cn>
2023-07-30 14:49:06 +0800

    feat(components/input): Exposes

--- packages/components/input/index.ts
+++ packages/components/input/index.ts
@@ -2,4 +2,5 @@ import Input from './src/input.vue'
 import { withInstall } from '@ltfei-blog/blogui-utils/vue/install'
 
 export const BInput = withInstall(Input)
+export * from './src/types'
 export default BInput
